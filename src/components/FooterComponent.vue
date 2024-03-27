1b1b4a9a2c584056d03775a85af96e51d4deb35e
mashirot <shiina@sakurasou.io>
2024-03-27 14:03:30 +0800

    feat: add bg img src

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="h-8 flex gap-4 bg-white opacity-90 justify-center align-middle rounded-lg">
+  <div class="h-8 flex gap-8 bg-white opacity-90 justify-center align-middle rounded-lg">
     <div class="leading-8">© 没良心的打吊针 2024-2024</div>
+    <a class="leading-8" href="https://www.bilibili.com/opus/878111166126096392" target="_blank"
+      >Background-Img-Src</a
+    >
   </div>
 </template>
