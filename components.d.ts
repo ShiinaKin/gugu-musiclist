0667ca3af8de8746c1d3319607f648a9d78afc62
mashirot <shiina@sakurasou.io>
2024-04-01 23:16:54 +0800

    feat: add back-2-top component

--- components.d.ts
+++ components.d.ts
@@ -5,19 +5,20 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 export {}
 
-declare module 'vue' {
+declare module "vue" {
   export interface GlobalComponents {
-    AvatarProfile: typeof import('./src/components/AvatarProfile.vue')['default']
-    FooterComponent: typeof import('./src/components/FooterComponent.vue')['default']
-    MusicLabel: typeof import('./src/components/MusicLabel.vue')['default']
-    MusicList: typeof import('./src/components/MusicList.vue')['default']
-    NButton: typeof import('naive-ui')['NButton']
-    NCard: typeof import('naive-ui')['NCard']
-    NDataTable: typeof import('naive-ui')['NDataTable']
-    NIcon: typeof import('naive-ui')['NIcon']
-    NInput: typeof import('naive-ui')['NInput']
-    NMessageProvider: typeof import('naive-ui')['NMessageProvider']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
+    AvatarProfile: (typeof import("./src/components/AvatarProfile.vue"))["default"];
+    FooterComponent: (typeof import("./src/components/FooterComponent.vue"))["default"];
+    MusicLabel: (typeof import("./src/components/MusicLabel.vue"))["default"];
+    MusicList: (typeof import("./src/components/MusicList.vue"))["default"];
+    NBackTop: (typeof import("naive-ui"))["NBackTop"];
+    NButton: (typeof import("naive-ui"))["NButton"];
+    NCard: (typeof import("naive-ui"))["NCard"];
+    NDataTable: (typeof import("naive-ui"))["NDataTable"];
+    NIcon: (typeof import("naive-ui"))["NIcon"];
+    NInput: (typeof import("naive-ui"))["NInput"];
+    NMessageProvider: (typeof import("naive-ui"))["NMessageProvider"];
+    RouterLink: (typeof import("vue-router"))["RouterLink"];
+    RouterView: (typeof import("vue-router"))["RouterView"];
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import FooterComponent from "./components/FooterComponent.vue";
         <MusicList />
         <FooterComponent class="mt-auto" />
       </div>
+      <n-back-top />
     </n-message-provider>
   </div>
 </template>
