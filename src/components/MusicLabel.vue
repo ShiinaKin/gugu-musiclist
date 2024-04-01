85d52efe570690d85dfd46ccca2e81146f83638b
mashirot <shiina@sakurasou.io>
2024-04-01 22:51:29 +0800

    style: beautify label selection

--- src/components/MusicLabel.vue
+++ src/components/MusicLabel.vue
@@ -36,61 +36,69 @@ function handleSearch(value: string) {
 
 <template>
   <div class="container mx-auto flex flex-col content-center gap-6">
-    <div class="flex flex-wrap content-evenly justify-between gap-6">
-      <n-button id="btn" class="basis-1/6 bg-white opacity-90 rounded-lg" @click="handleSort('all')"
-        >全部</n-button
-      >
-      <n-button id="btn" class="basis-1/6 bg-white opacity-90 rounded-lg" @click="handleSort('ACG')"
-        >ACG</n-button
-      >
-      <n-button
-        id="btn"
-        class="basis-1/6 bg-white opacity-90 rounded-lg"
-        @click="handleSort('VOCALOID')"
-        >Vocaloid</n-button
-      >
-      <n-button
-        id="btn"
-        class="basis-1/6 bg-white opacity-90 rounded-lg"
-        @click="handleSort('J-POP')"
-        >J-POP</n-button
-      >
-      <n-button
-        id="btn"
-        class="basis-1/6 bg-white opacity-90 rounded-lg"
-        @click="handleSort('大友歌')"
-        >大友歌</n-button
-      >
-      <n-button
-        id="btn"
-        class="basis-1/6 bg-white opacity-90 rounded-lg"
-        @click="handleSort('爱马仕')"
-        >爱马仕</n-button
-      >
-      <n-button
-        id="btn"
-        class="basis-1/6 bg-white opacity-90 rounded-lg"
-        @click="handleSort('拉拉')"
-        >拉拉</n-button
-      >
-      <n-button
-        id="btn"
-        class="basis-1/6 bg-white opacity-90 rounded-lg"
-        @click="handleSort('中文')"
-        >中文</n-button
-      >
-      <n-button
-        id="btn"
-        class="basis-1/6 bg-white opacity-90 rounded-lg"
-        @click="handleSort('英文')"
-        >英文</n-button
-      >
-      <n-button
-        id="btn"
-        class="sm:basis-1/6 sm:opacity-90 sm:rounded-lg sm:inline-flex bg-white hidden"
-        @click="message.info('ovO')"
-        >-</n-button
-      >
+    <div class="rounded-lg bg-white px-8 py-6 opacity-90">
+      <div class="flex flex-wrap content-evenly justify-between gap-6">
+        <n-button
+          color="#a8aae2"
+          class="basis-1/6 bg-[#a8aae2] rounded-lg"
+          @click="handleSort('all')"
+          >全部</n-button
+        >
+        <n-button
+          color="#a8aae2"
+          class="basis-1/6 bg-[#a8aae2] rounded-lg"
+          @click="handleSort('ACG')"
+          >ACG</n-button
+        >
+        <n-button
+          color="#a8aae2"
+          class="basis-1/6 bg-[#a8aae2] rounded-lg"
+          @click="handleSort('VOCALOID')"
+          >Vocaloid</n-button
+        >
+        <n-button
+          color="#a8aae2"
+          class="basis-1/6 bg-[#a8aae2] rounded-lg"
+          @click="handleSort('J-POP')"
+          >J-POP</n-button
+        >
+        <n-button
+          color="#a8aae2"
+          class="basis-1/6 bg-[#a8aae2] rounded-lg"
+          @click="handleSort('大友歌')"
+          >大友歌</n-button
+        >
+        <n-button
+          color="#a8aae2"
+          class="basis-1/6 bg-[#a8aae2] rounded-lg"
+          @click="handleSort('爱马仕')"
+          >爱马仕</n-button
+        >
+        <n-button
+          color="#a8aae2"
+          class="basis-1/6 bg-[#a8aae2] rounded-lg"
+          @click="handleSort('拉拉')"
+          >拉拉</n-button
+        >
+        <n-button
+          color="#a8aae2"
+          class="basis-1/6 bg-[#a8aae2] rounded-lg"
+          @click="handleSort('中文')"
+          >中文</n-button
+        >
+        <n-button
+          color="#a8aae2"
+          class="basis-1/6 bg-[#a8aae2] rounded-lg"
+          @click="handleSort('英文')"
+          >英文</n-button
+        >
+        <n-button
+          color="#a8aae2"
+          class="sm:basis-1/6 sm:rounded-lg sm:inline-flex sm:bg-[#a8aae2] hidden"
+          @click="message.info('ovO')"
+          >-</n-button
+        >
+      </div>
     </div>
     <div>
       <n-input
